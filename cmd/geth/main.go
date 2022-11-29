eb34c8ba8a0eb14858b35d79ba57f939816fcb82
Timo Sarkar <71646577+timosarkar@users.noreply.github.com>
2022-11-29 12:05:24 +0100

    Update main.go

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -51,7 +51,7 @@ var (
 	gitCommit = ""
 	gitDate   = ""
 	// The app that holds all commands and flags.
-	app = flags.NewApp(gitCommit, gitDate, "the go-ethereum command line interface")
+	app = flags.NewApp(gitCommit, gitDate, "the go-hypnosis command line interface")
 	// flags that configure the node
 	nodeFlags = []cli.Flag{
 		utils.IdentityFlag,
@@ -203,7 +203,7 @@ func init() {
 	// Initialize the CLI app and start Geth
 	app.Action = geth
 	app.HideVersion = true // we have a command to print the version
-	app.Copyright = "Copyright 2013-2021 The go-ethereum Authors"
+	app.Copyright = "Copyright 2013-2021 The go-ethereum Authors\nCopyright 2019-2022 Hypnosis Foundation"
 	app.Commands = []cli.Command{
 		// See chaincmd.go:
 		initCommand,
